c90c69d2bc2425794ce167cfc3fd8cffec1b02e5
Guilherme <79217697+TIOshaka@users.noreply.github.com>
2023-11-22 08:08:43 -0300

    Update inserir_bd.php

--- Trabalho/projetophp/inserir_bd.php
+++ Trabalho/projetophp/inserir_bd.php
@@ -1,7 +1,5 @@
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -22,7 +20,9 @@
                 </ul>
             </div>
         </nav>
-        <?php
+        <body> <section class="palestrantes-icons">
+            <div class="palestrante">
+            <?php
                 $servername = "localhost";
                 $username = "root";
                 $password = "";
@@ -42,42 +42,49 @@
                     $cpf = $_POST["cpf"];
                     $senha = password_hash($_POST["senha"], PASSWORD_DEFAULT);
 
-
                     $sql = "INSERT INTO clientes (nome, email, cep, numero, cpf, senha) VALUES ('$nome', '$email', '$cep', '$telefone', '$cpf', '$senha')";
-
                     
-                    
+                    echo '<div class="mensagem-sucesso">';
+                    echo '<p style="font-size: 20px; font-weight: bold;">Obrigado pela sua inscrição, ' . $nome . '!</p>';
+                    echo '</div>';                    
                     if ($conn->query($sql) === TRUE) {
                         $last_id = $conn->insert_id;
             
-                        echo "Sua inscrição foi realizada com sucesso. Seu número de inscrição é: $last_id";
+                        echo '<div class="mensagem-sucesso">';
+                        echo '<p style="font-size: 18px;">Sua inscrição foi realizada com sucesso. Seu número de inscrição é: <span style="color: #0099ff;">' . $last_id . '</span>, apresente esse código na entrada do evento.</p><br>';
+                        echo '<p style="font-size: 14px;">Caso deseje fazer alguma alteração nos dados fornecidos, entre em contato com o email: contatotechfest@gmail.com.</p>';
+                        echo '</div>';
+                        
                         
                     } else {
                         echo "Erro ao realizar a inscrição: " . $conn->error;
                     }
-
                     
                 }
                 $conn->close();
         ?>
-        <section class="footer">
+            </div>
+    </section>
+</body>
+    </section>
+
+    <section class="footer">
         <footer>
             <img class="logof" src="assets/logo.png">
-            
             <div class="opfoot">
                 <ul>
-                    <li><a class="#" href="sobrenos.html">Sobre Nós</a></li>
+                <li><a class="#" href="sobrenos.html">Sobre Nós</a></li>
                 </ul>
             </div>
-            <div class="infos"></div> 
-                <p>E-mail: contatotechfest@gmail.com </p>
+            <div class="infos">
+                <p>E-mail: contatotechfest@gmail.com</p>
                 <p>Endereço: BR-230 - Água Fria, João Pessoa - PB, 58053-000</p>
-                <h3>Siga-nos Nas Redes Sociais:</h3>
-                <div class="redes">
-                    <a href="#" class="icones"><img class="twitter" src="https://cdn-icons-png.flaticon.com/512/124/124021.png" alt="Twitter"></a>
-                    <a href="#" class="icones"><img class="instagram" src="https://img.freepik.com/vetores-premium/icone-do-instagram-de-logotipo-de-distintivo-moderno_578229-124.jpg" alt="Instagram"></a>
-                    <a href="#" class="icones"><img class="youtube" src="https://img.freepik.com/vetores-premium/logo-vermelho-do-youtube-logo-de-midia-social_197792-1803.jpg" alt="YouTube"></a>
-                </div>
+            </div>
+            <h3>Siga-nos Nas Redes Sociais:</h3>
+            <div class="redes">
+                <a href="#" class="icones"><img class="twitter" src="https://cdn-icons-png.flaticon.com/512/124/124021.png" alt="Twitter"></a>
+                <a href="#" class="icones"><img class="instagram" src="https://img.freepik.com/vetores-premium/icone-do-instagram-de-logotipo-de-distintivo-moderno_578229-124.jpg" alt="Instagram"></a>
+                <a href="#" class="icones"><img class="youtube" src="https://img.freepik.com/vetores-premium/logo-vermelho-do-youtube-logo-de-midia-social_197792-1803.jpg" alt="YouTube"></a>
             </div>
         </footer>
     </section>
